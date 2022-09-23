986a6b67635b36c9b87193500c9c93c98f01df2d
VilosmZX <jravaell@gmail.com>
2022-09-23 19:57:22 +0700

    added prisma to req.prisma

--- src/Api/Server.ts
+++ src/Api/Server.ts
@@ -2,8 +2,9 @@ import { WASocket } from '@adiwajshing/baileys';
 import express from 'express';
 import router from './Router/Router';
 import cors from 'cors';
+import { PrismaClient } from '@prisma/client';
 
-export default async function(client: WASocket) {
+export default async function(client: WASocket, prisma: PrismaClient) {
     const app = express();
     app.use(express.json());
     app.use(cors({
@@ -12,6 +13,7 @@ export default async function(client: WASocket) {
     }))
     app.use((req, res, next) => {
         req.client = client;
+        req.prisma = prisma;
         next();
     })
     app.use('/api/client', router);
