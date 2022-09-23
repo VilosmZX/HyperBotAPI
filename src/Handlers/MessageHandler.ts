3eeacb73951f412572ae2b8d6816fa791d79b524
VilosmZX <jravaell@gmail.com>
2022-09-23 19:58:05 +0700

    Check for autoreply commands

--- src/Handlers/MessageHandler.ts
+++ src/Handlers/MessageHandler.ts
@@ -1,8 +1,12 @@
 import { WASocket, BaileysEventEmitter } from "@adiwajshing/baileys";
+import { PrismaClient } from '@prisma/client';
 
-export default async function(event: BaileysEventEmitter) {
-    event.on('messages.upsert', async ({ messages }) => {
+export default async function(client: WASocket, prisma: PrismaClient) {
+    client.ev.on('messages.upsert', async ({ messages }) => {
         const m = messages[0];
-        console.log(m);
+        const content = messages[0].message?.conversation as string;
+        const reply = await prisma.autoReply.findFirst({ where: { trigger: content } });
+        if (reply !== null)
+            await client.sendMessage(m.key.remoteJid!, { text: reply.reply });
     })
 }
