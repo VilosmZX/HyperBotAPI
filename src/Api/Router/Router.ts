676f630efe68fc2c44e530e5ed0ffa20780284d7
VilosmZX <jravaell@gmail.com>
2022-09-23 19:21:16 +0700

    Added prisma and @prisma/client

--- src/Api/Router/Router.ts
+++ src/Api/Router/Router.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { isString } from '../../Utils/Utils';
+import { isGroupExists } from '../../Utils/Utils';
 
 const router = Router();
 
@@ -14,14 +14,19 @@ interface Test {
 
 router.get('/chats/:jid', async (req, res) => {
     const jid = req.params.jid as string;
-    const chat = await req.client.groupMetadata(jid);
-    const arrayBuffer = (chat.desc as unknown as Test).data;
-    res.status(200).json(Buffer.from(arrayBuffer));
+    const chat = await req.client.groupMetadata(jid) || null;
+    if (await isGroupExists(req.client, jid))
+        return res.status(200).json(chat)
+    res.status(404).json({error: 'Group not found!'});
 });
 
 router.post('/chats/:jid/send', async (req, res) => {
     const jid = req.params.jid;
     const msg = req.query.msg as string;
+    
+    if (!(await isGroupExists(req.client, jid)))
+        return res.sendStatus(404);
+
     if (msg) {
         await req.client.sendMessage(jid, { text: msg });
         return res.sendStatus(200);
--- src/Utils/Utils.ts
+++ src/Utils/Utils.ts
@@ -1,3 +1,12 @@
+import { WASocket } from "@adiwajshing/baileys";
+
 export function isString(value: any): boolean {
     return typeof value === 'string';
+}
+
+export async function isGroupExists(client: WASocket, jid: string): Promise<boolean> {
+    const chat = await client.groupMetadata(jid);
+    if (chat.id)
+        return true;
+    return false;
 }
