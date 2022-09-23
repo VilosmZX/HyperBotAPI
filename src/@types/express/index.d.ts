5e9b3a88315ff70120a4e7df5ecb7ffe38cef58f
VilosmZX <jravaell@gmail.com>
2022-09-23 19:57:10 +0700

    Added prisma to Request interface express

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -1,8 +1,10 @@
 import { WASocket } from "@adiwajshing/baileys";
 import { Request } from "express-serve-static-core";
+import { PrismaClient } from '@prisma/client';
 
 declare module 'express-serve-static-core' {
     interface Request {
         client: WASocket;
+        prisma: PrismaClient;
     }
 }
