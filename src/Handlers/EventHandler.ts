c610f6922a1127a35b99144f3357867ab28c3d47
VilosmZX <jravaell@gmail.com>
2022-09-25 18:18:53 +0700

    Added lastDisconnect reason

--- src/Handlers/EventHandler.ts
+++ src/Handlers/EventHandler.ts
@@ -2,11 +2,11 @@ import { WASocket } from "@adiwajshing/baileys";
 
 export default async function(client: WASocket, saveCreds: () => Promise<void>) {
     client.ev.on('connection.update', async update => {
-        const { connection } = update;
+        const { connection, lastDisconnect } = update;
         if (connection === 'open')
             console.log('Bot is online!')
         else if (connection === 'close')
-            console.log('Bot is offline');
+            console.log(lastDisconnect?.error?.stack);
     });
     client.ev.on('creds.update', saveCreds);
 }
