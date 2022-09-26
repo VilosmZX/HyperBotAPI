25e5276940609820f62e87da52d46500e65e3589
VilosmZX <jravaell@gmail.com>
2022-09-26 17:07:30 +0700

    Connect again if disconnect not by user

--- src/Handlers/EventHandler.ts
+++ src/Handlers/EventHandler.ts
@@ -1,12 +1,17 @@
-import { WASocket } from "@adiwajshing/baileys";
+import { WASocket, DisconnectReason } from "@adiwajshing/baileys";
+import boom from 'boom';
 
-export default async function(client: WASocket, saveCreds: () => Promise<void>) {
+export default async function(client: WASocket, saveCreds: () => Promise<void>, connectToWa: () => Promise<void>) {
     client.ev.on('connection.update', async update => {
         const { connection, lastDisconnect } = update;
         if (connection === 'open')
             console.log('Bot is online!')
-        else if (connection === 'close')
-            console.log(lastDisconnect?.error?.stack);
+        else if (connection === 'close') {
+            const shouldReconnect = (lastDisconnect?.error as boom).output.statusCode !== DisconnectReason.loggedOut;
+            if (shouldReconnect)
+                await connectToWa();
+
+        }
     });
     client.ev.on('creds.update', saveCreds);
 }
